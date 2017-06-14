2a1690289627e5a006cbdcbff704fd8752aa8b78
Bikash <bikash11roy@gmail.com>
2017-06-15 00:08:40 +0600

    Android Mnist

---
+++ Android_Mnist/app/src/main/java/com/example/bikash/android_mnist/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.bikash.android_mnist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
